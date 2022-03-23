716e70bf12f0020bd0cc6050ca916c4ae44cffa0
Rastislav <rasto.kopal@gmail.com>
2022-03-23 19:39:47 +0100

    oops:)

--- src/api/jobs/jobs.js
+++ src/api/jobs/jobs.js
@@ -2,15 +2,15 @@
 // const updateItemTask = require('./tasks/updateItemsTask');
 // const tokenInfoCrawlerTask = require('./tasks/tokenInfoCrawlerTask');
 
-// const collectionTimestampTask = require('./tasks/collectionTimestampTask');
-// const updateItemsTask = require('./tasks/updateItemsTask');
+const collectionTimestampTask = require('./tasks/collectionTimestampTask');
+const updateItemsTask = require('./tasks/updateItemsTask');
 const updateHoldersTask = require('./tasks/updateHolders.task');
-// const getBuyTransactionsTask = require('./tasks/getBuyTransactionsTask');
+const getBuyTransactionsTask = require('./tasks/getBuyTransactionsTask');
 
 const tasks = {
-  // collectionTimestampTask, // dedicated VM is running this job :)
-  // updateItemsTask,
-  // getBuyTransactionsTask,
+  collectionTimestampTask, // dedicated VM is running this job :)
+  updateItemsTask,
+  getBuyTransactionsTask,
   updateHoldersTask,
 };
 
