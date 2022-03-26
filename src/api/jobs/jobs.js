9e6876671ae6b55ed70c8259d03e03ebc99576da
Rastislav <rasto.kopal@gmail.com>
2022-03-26 18:48:30 +0100

    pushed commented changes...

--- src/api/jobs/jobs.js
+++ src/api/jobs/jobs.js
@@ -3,15 +3,15 @@
 // const tokenInfoCrawlerTask = require('./tasks/tokenInfoCrawlerTask');
 
 const collectionTimestampTask = require('./tasks/collectionTimestampTask');
-// const updateItemsTask = require('./tasks/updateItemsTask');
-// const updateHoldersTask = require('./tasks/updateHolders.task');
-// const getBuyTransactionsTask = require('./tasks/getBuyTransactionsTask');
+const updateItemsTask = require('./tasks/updateItemsTask');
+const updateHoldersTask = require('./tasks/updateHolders.task');
+const getBuyTransactionsTask = require('./tasks/getBuyTransactionsTask');
 
 const tasks = {
   collectionTimestampTask, // dedicated VM is running this job :)
-  // updateItemsTask,
-  // getBuyTransactionsTask,
-  // updateHoldersTask,
+  updateItemsTask,
+  getBuyTransactionsTask,
+  updateHoldersTask,
 };
 
 module.exports = tasks;
