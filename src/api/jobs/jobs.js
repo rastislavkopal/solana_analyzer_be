05c250997f9abe07f365264aa3ed14796453c659
Daniel Jankech <djankech98@gmail.com>
2022-04-22 14:56:44 +0200

    fixed updateholderstask bug

--- src/api/jobs/jobs.js
+++ src/api/jobs/jobs.js
@@ -2,18 +2,18 @@
 // const updateItemTask = require('./tasks/updateItemsTask');
 // const tokenInfoCrawlerTask = require('./tasks/tokenInfoCrawlerTask');
 
-//const collectionTimestampTask = require('./tasks/collectionTimestampTask');
+const collectionTimestampTask = require('./tasks/collectionTimestampTask');
 const updateItemsTask1hr = require('./tasks/updateItemsTask1hr');
 const updateItemsTask1m = require('./tasks/updateItemsTask1m');
-//const updateHoldersTask = require('./tasks/updateHolders.task');
-//const getBuyTransactionsTask = require('./tasks/getBuyTransactionsTask');
+const updateHoldersTask = require('./tasks/updateHolders.task');
+const getBuyTransactionsTask = require('./tasks/getBuyTransactionsTask');
 
 const tasks = {
- // collectionTimestampTask, // dedicated VM is running this job :)
+  collectionTimestampTask, // dedicated VM is running this job :)
   updateItemsTask1hr,
   updateItemsTask1m,
-//  getBuyTransactionsTask,
-//  updateHoldersTask,
+  getBuyTransactionsTask,
+  updateHoldersTask,
 };
 
 module.exports = tasks;
